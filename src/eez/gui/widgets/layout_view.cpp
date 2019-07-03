68a409b81e2e40b2ea59f85c3176045a7dbcfbfb
Martin Vladic <martin.vladic@gmail.com>
2019-07-03 12:30:59 +0200

    fixed crash when layout view is not specified

--- src/eez/gui/widgets/layout_view.cpp
+++ src/eez/gui/widgets/layout_view.cpp
@@ -29,9 +29,11 @@ void LayoutViewWidget_enum(OBJ_OFFSET widgetOffset, int16_t x, int16_t y, data::
                            EnumWidgetsCallback callback) {
     DECL_WIDGET(widget, widgetOffset);
     DECL_WIDGET_SPECIFIC(LayoutViewWidgetSpecific, layoutViewSpecific, widget);
-    DECL_WIDGET(layout, getPageOffset(layoutViewSpecific->layout));
-    DECL_WIDGET_SPECIFIC(PageWidget, layoutSpecific, layout);
-    enumContainer(x, y, cursor, previousState, currentState, callback, layoutSpecific->widgets);
+	if (layoutViewSpecific->layout != -1) {
+		DECL_WIDGET(layout, getPageOffset(layoutViewSpecific->layout));
+		DECL_WIDGET_SPECIFIC(PageWidget, layoutSpecific, layout);
+		enumContainer(x, y, cursor, previousState, currentState, callback, layoutSpecific->widgets);
+	}
 }
 
 } // namespace gui
