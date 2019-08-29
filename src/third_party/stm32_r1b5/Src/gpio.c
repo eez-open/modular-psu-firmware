88e45ebea4cbccb0ea61e20e829e15414e3e8009
Martin Vladic <martin.vladic@gmail.com>
2019-08-29 09:51:57 +0200

    pullup on enc sw

--- src/third_party/stm32_r1b5/Src/gpio.c
+++ src/third_party/stm32_r1b5/Src/gpio.c
@@ -187,7 +187,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = ENC_SW_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(ENC_SW_GPIO_Port, &GPIO_InitStruct);
 
 }
