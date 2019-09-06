a29263bcb61f9d81a44305896a68bacf99db1eb3
Martin Vladic <martin.vladic@gmail.com>
2019-09-06 10:04:04 +0200

    PWR_SSTART_Pin and PWR_DIRECT_Pin pull down r1b5

--- src/third_party/stm32_r1b5/Src/gpio.c
+++ src/third_party/stm32_r1b5/Src/gpio.c
@@ -110,7 +110,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pins : PGPin PGPin */
   GPIO_InitStruct.Pin = PWR_DIRECT_Pin|PWR_SSTART_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
 
