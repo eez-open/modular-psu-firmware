b4753ad3de09dd3a0b9967b1894083b4ed2e2a7e
Martin Vladic <martin.vladic@gmail.com>
2020-10-13 16:03:55 +0200

    Update conf_advanced.h

--- src/eez/modules/psu/conf_advanced.h
+++ src/eez/modules/psu/conf_advanced.h
@@ -23,7 +23,7 @@
 #pragma once
 
 /// MCU firmware version.
-#define MCU_FIRMWARE "1.3.1"
+#define MCU_FIRMWARE "1.3.2"
 
 /// Manufacturer description text used for *IDN?
 #define IDN_MANUFACTURER "Envox"
