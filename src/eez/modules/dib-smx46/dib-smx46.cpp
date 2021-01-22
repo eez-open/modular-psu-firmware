bfdba65f97c3bf663e66fa7547780cc82fc195f9
Martin Vladic <martin.vladic@gmail.com>
2021-01-23 00:13:21 +0100

    sm46 master slave communication

--- src/eez/modules/dib-smx46/dib-smx46.cpp
+++ src/eez/modules/dib-smx46/dib-smx46.cpp
@@ -76,25 +76,116 @@ static const int NUM_ROWS = 4;
 
 static const uint32_t MIN_TO_MS = 60L * 1000L;
 
-struct FromMasterToSlave {
+////////////////////////////////////////////////////////////////////////////////
+
+static const int NUM_REQUEST_RETRIES = 100;
+static const int MAX_DMA_TRANSFER_ERRORS = 100;
+static const uint32_t REFRESH_TIME_MS = 250;
+static const uint32_t TIMEOUT_TIME_MS = 350;
+static const uint32_t TIMEOUT_UNTIL_OUT_OF_SYNC_MS = 10000;
+
+enum Command {
+	COMMAND_NONE       = 0x113B3759,
+    COMMAND_GET_INFO   = 0x21EC18D4,
+    COMMAND_GET_STATE  = 0x3C1D2EF4,
+    COMMAND_SET_PARAMS = 0x4B723BFF
+};
+
+struct SetParams {
     uint32_t routes;
     float dac1;
     float dac2;
     uint8_t relayOn;
 };
 
-struct FromSlaveToMaster {
+struct Request {
+	uint32_t command;
+
+    union {
+    	SetParams setParams;
+    };
 };
+
+struct Response {
+	uint32_t command;
+
+    union {
+        struct {
+            uint8_t firmwareMajorVersion;
+            uint8_t firmwareMinorVersion;
+            uint32_t idw0;
+            uint32_t idw1;
+            uint32_t idw2;
+        } getInfo;
+
+        struct {
+            uint32_t tickCount;
+        } getState;
+
+        struct {
+            uint8_t result; // 1 - success, 0 - failure
+        } setParams;
+    };
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define BUFFER_SIZE 20
+
+////////////////////////////////////////////////////////////////////////////////
 
 struct Smx46Module : public Module {
 public:
     TestResult testResult = TEST_NONE;
+
     bool synchronized = false;
-    int numCrcErrors = 0;
-    uint32_t lastTransferTickCount;
-    uint8_t input[BUFFER_SIZE];
-    uint8_t output[BUFFER_SIZE];
+
+    ////////////////////////////////////////
+    uint32_t input[(BUFFER_SIZE + 3) / 4 + 1];
+    uint32_t output[(BUFFER_SIZE + 3) / 4];
+
     bool spiReady = false;
+    bool spiDmaTransferCompleted = false;
+    int spiDmaTransferStatus;
+
+    uint32_t lastTransferTime = 0;
+	SetParams lastTransferredParams;
+
+	struct CommandDef {
+		uint32_t command;
+		void (Smx46Module::*fillRequest)(Request &request);
+		void (Smx46Module::*done)(Response &response, bool isSuccess);
+	};
+
+    static const CommandDef getInfo_command;
+    static const CommandDef getState_command;
+    static const CommandDef setParams_command;
+
+    enum State {
+        STATE_IDLE,
+
+        STATE_WAIT_SLAVE_READY_BEFORE_REQUEST,
+        STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_REQUEST,
+
+        STATE_WAIT_SLAVE_READY_BEFORE_RESPONSE,
+        STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_RESPONSE
+    };
+
+    enum Event {
+        EVENT_SLAVE_READY,
+        EVENT_DMA_TRANSFER_COMPLETED,
+        EVENT_DMA_TRANSFER_FAILED,
+        EVENT_TIMEOUT
+    };
+
+    const CommandDef *currentCommand = nullptr;
+    uint32_t refreshStartTime;
+    State state;
+    uint32_t lastStateTransitionTime;
+    uint32_t lastRefreshTime;
+    int retry;
+    ////////////////////////////////////////
+
 
     uint32_t routes;
 
@@ -125,7 +216,7 @@ public:
         latestModuleRevision = MODULE_REVISION_R1B2;
         flashMethod = FLASH_METHOD_STM32_BOOTLOADER_UART;
 #if defined(EEZ_PLATFORM_STM32)        
-        spiBaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+        spiBaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
         spiCrcCalculationEnable = true;
 #else
         spiBaudRatePrescaler = 0;
@@ -133,15 +224,12 @@ public:
 #endif
         numPowerChannels = 0;
         numOtherChannels = 2 + 1 + NUM_ROWS * NUM_COLUMNS;
-        isResyncSupported = false;
+        isResyncSupported = true;
 
         resetConfiguration();
 
-        auto data = (FromMasterToSlave *)output;
-        data->routes = routes;
-        data->dac1 = dac1;
-        data->dac2 = dac2;
-        data->relayOn = relayOn;
+        memset(input, 0, sizeof(input));
+        memset(output, 0, sizeof(output));
     }
 
     void boot() override {
@@ -163,105 +251,352 @@ public:
 
     void initChannels() override {
         if (!synchronized) {
-            if (bp3c::comm::masterSynchro(slotIndex)) {
-                synchronized = true;
-                numCrcErrors = 0;
-                lastTransferTickCount = millis();
-                testResult = TEST_OK;
-            } else {
-                if (g_slots[slotIndex]->firmwareInstalled) {
-                    event_queue::pushEvent(event_queue::EVENT_ERROR_SLOT1_SYNC_ERROR + slotIndex);
-                }
-                testResult = TEST_FAILED;
+			executeCommand(&getInfo_command);
+
+			if (!g_isBooted) {
+				while (state != STATE_IDLE) {
+                    WATCHDOG_RESET(WATCHDOG_LONG_OPERATION);
+
+#if defined(EEZ_PLATFORM_STM32)
+					if (HAL_GPIO_ReadPin(spi::IRQ_GPIO_Port[slotIndex], spi::IRQ_Pin[slotIndex]) == GPIO_PIN_RESET) {
+                        osDelay(1);
+                        if (HAL_GPIO_ReadPin(spi::IRQ_GPIO_Port[slotIndex], spi::IRQ_Pin[slotIndex]) == GPIO_PIN_RESET) {
+						    spiReady = true;
+                        }
+					}
+#endif
+					tick();
+				}
+			}
+
+            memset(&lastTransferredParams, 0, sizeof(SetParams));
+        }
+    }
+
+    ////////////////////////////////////////
+
+    void Command_GetInfo_Done(Response &response, bool isSuccess) {
+        if (isSuccess) {
+            auto &data = response.getInfo;
+
+            firmwareMajorVersion = data.firmwareMajorVersion;
+            firmwareMinorVersion = data.firmwareMinorVersion;
+            idw0 = data.idw0;
+            idw1 = data.idw1;
+            idw2 = data.idw2;
+
+			firmwareVersionAcquired = true;
+
+            synchronized = true;
+            testResult = TEST_OK;
+        } else {
+            synchronized = false;
+            if (firmwareInstalled) {
+                event_queue::pushEvent(event_queue::EVENT_ERROR_SLOT1_SYNC_ERROR + slotIndex);
+            }
+            testResult = TEST_FAILED;
+        }
+    }
+
+    ////////////////////////////////////////
+
+    void Command_GetState_Done(Response &response, bool isSuccess) {
+        if (isSuccess) {
+            lastRefreshTime = refreshStartTime;
+        }
+    }
+
+    ////////////////////////////////////////
+
+	void fillSetParams(SetParams &params) {
+		memset(&params, 0, sizeof(SetParams));
+
+		params.routes = routes;
+        params.dac1 = calibrationEnabled[0] && isVoltageCalibrationExists(0) ? calibration::remapValue(dac1, calConf[0].u) : dac1;
+        params.dac2 = calibrationEnabled[1] && isVoltageCalibrationExists(1) ? calibration::remapValue(dac2, calConf[1].u) : dac2;
+        params.relayOn = relayOn ? 1 : 0;
+	}
+
+    void Command_SetParams_FillRequest(Request &request) {
+        fillSetParams(request.setParams);
+    }
+
+    void Command_SetParams_Done(Response &response, bool isSuccess) {
+        if (isSuccess) {
+            auto &data = response.setParams;
+            if (data.result) {
+                SetParams &params = ((Request *)output)->setParams;
+
+                updateRelayCycles(lastTransferredParams.routes, params.routes, lastTransferredParams.relayOn, params.relayOn);
+
+                memcpy(&lastTransferredParams, &params, sizeof(SetParams));
             }
         }
     }
 
-    void tick() override {
-        if (!synchronized) {
-            return;
+    ////////////////////////////////////////
+
+	uint32_t getRefreshTimeMs() {
+		return REFRESH_TIME_MS;
+	}
+
+    void executeCommand(const CommandDef *command) {
+        currentCommand = command;
+        retry = 0;
+        setState(STATE_WAIT_SLAVE_READY_BEFORE_REQUEST);
+	}
+
+    bool startCommand() {
+		Request &request = *(Request *)output;
+
+		request.command = currentCommand->command;
+
+		if (currentCommand->fillRequest) {
+			(this->*currentCommand->fillRequest)(request);
+		}
+        spiReady = false;
+        spiDmaTransferCompleted = false;
+        auto status = bp3c::comm::transferDMA(slotIndex, (uint8_t *)output, (uint8_t *)input, BUFFER_SIZE);
+        return status == bp3c::comm::TRANSFER_STATUS_OK;
+    }
+
+    bool getCommandResult() {
+        Request &request = *(Request *)output;
+        request.command = COMMAND_NONE;
+        spiReady = false;
+        spiDmaTransferCompleted = false;
+        auto status = bp3c::comm::transferDMA(slotIndex, (uint8_t *)output, (uint8_t *)input, BUFFER_SIZE);
+        return status == bp3c::comm::TRANSFER_STATUS_OK;
+    }
+
+    bool isCommandResponse() {
+        Response &response = *(Response *)input;
+        return response.command == (0x8000 | currentCommand->command);
+    }
+
+    void doRetry() {
+    	bp3c::comm::abortTransfer(slotIndex);
+        
+        if (++retry < NUM_REQUEST_RETRIES) {
+            // try again
+            setState(STATE_WAIT_SLAVE_READY_BEFORE_REQUEST);
+        } else {
+            // give up
+            doCommandDone(false);
+        }
+    }
+
+    void doCommandDone(bool isSuccess) {
+        if (isSuccess) {
+            lastTransferTime = millis();
         }
 
-#if defined(EEZ_PLATFORM_STM32)
-        if (spiReady) {
-#endif
-            FromMasterToSlave data;
+		if (currentCommand->done) {
+			Response &response = *(Response *)input;
+			(this->*currentCommand->done)(response, isSuccess);
+		}
 
-            uint32_t newRoutes = routes;
-            uint8_t newRelayOn = relayOn ? 1 : 0;
+		currentCommand = nullptr;
+        setState(STATE_IDLE);
+    }
 
-            data.routes = newRoutes;
-            data.dac1 = calibrationEnabled[0] && isVoltageCalibrationExists(0) ? calibration::remapValue(dac1, calConf[0].u) : dac1;
-            data.dac2 = calibrationEnabled[1] && isVoltageCalibrationExists(1) ? calibration::remapValue(dac2, calConf[1].u) : dac2;
-            data.relayOn = newRelayOn;
+    void setState(State newState) {
+        state = newState;
+        lastStateTransitionTime = millis();
+    }
 
-            if (memcmp(output, &data, sizeof(FromMasterToSlave)) != 0) {
-                FromMasterToSlave *oldData = (FromMasterToSlave *)output;
-
-                uint32_t oldRoutes = oldData->routes;
-                uint8_t oldRelayOn = oldData->relayOn;
-
-            	memcpy(output, &data, sizeof(FromMasterToSlave));
-                if (transfer()) {
-                    updateRelayCycles(oldRoutes, newRoutes, oldRelayOn, newRelayOn);
+    void stateTransition(Event event) {
+    	if (event == EVENT_DMA_TRANSFER_COMPLETED) {
+            numCrcErrors = 0;
+            numTransferErrors = 0;
+    	}
+ 
+        if (state == STATE_WAIT_SLAVE_READY_BEFORE_REQUEST) {
+            if (event == EVENT_TIMEOUT) {
+				doRetry();
+            } else if (event == EVENT_SLAVE_READY) {
+                if (startCommand()) {
+                    setState(STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_REQUEST);
+                } else {
+                    doRetry();
                 }
-#if defined(EEZ_PLATFORM_STM32)
-                spiReady = false;
-#endif
             }
-#if defined(EEZ_PLATFORM_STM32)
+        } else if (state == STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_REQUEST) {
+            if (event == EVENT_TIMEOUT) {
+                doRetry();
+            } else if (event == EVENT_DMA_TRANSFER_COMPLETED) {
+                setState(STATE_WAIT_SLAVE_READY_BEFORE_RESPONSE);
+            } else if (event == EVENT_DMA_TRANSFER_FAILED) {
+                doRetry();
+            }
+        } else if (state == STATE_WAIT_SLAVE_READY_BEFORE_RESPONSE) {
+            if (event == EVENT_TIMEOUT) {
+				doRetry();
+            } else if (event == EVENT_SLAVE_READY) {
+                if (getCommandResult()) {
+                    setState(STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_RESPONSE);
+                } else {
+                    doRetry();
+                }
+            }
+        } else if (state == STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_RESPONSE) {
+            if (event == EVENT_TIMEOUT) {
+                doRetry();
+            } else if (event == EVENT_DMA_TRANSFER_COMPLETED) {
+                if (isCommandResponse()) {
+                    doCommandDone(true);
+                } else {
+                    doRetry();
+                }
+            } else if (event == EVENT_DMA_TRANSFER_FAILED) {
+                doRetry();
+            }
+        } 
+    }
+
+    int numCrcErrors = 0;
+    int numTransferErrors = 0;
+
+    void reportDmaTransferFailed(int status) {
+        if (status == bp3c::comm::TRANSFER_STATUS_CRC_ERROR) {
+            numCrcErrors++;
+            if (numCrcErrors >= MAX_DMA_TRANSFER_ERRORS) {
+                event_queue::pushEvent(event_queue::EVENT_ERROR_SLOT1_CRC_CHECK_ERROR + slotIndex);
+                synchronized = false;
+                testResult = TEST_FAILED;
+            }
+            //else if (numCrcErrors > 5) {
+            //    DebugTrace("Slot %d CRC error no. %d\n", slotIndex + 1, numCrcErrors);
+            //}
         } else {
-            int32_t diff = millis() - lastTransferTickCount;
-            if (diff > CONF_TRANSFER_TIMEOUT_MS) {
+            numTransferErrors++;
+            if (numTransferErrors >= MAX_DMA_TRANSFER_ERRORS) {
                 event_queue::pushEvent(event_queue::EVENT_ERROR_SLOT1_SYNC_ERROR + slotIndex);
                 synchronized = false;
                 testResult = TEST_FAILED;
             }
+            //else if (numTransferErrors > 5) {
+            //    DebugTrace("Slot %d SPI transfer error %d no. %d\n", slotIndex + 1, status, numTransferErrors);
+            //}
         }
-#endif
+    }
 
+    void tick() override {
         if (relayCyclesWriteInterval.test()) {
             sendMessageToLowPriorityThread((LowPriorityThreadMessage)THREAD_MESSAGE_SAVE_RELAY_CYCLES, slotIndex);
         }
-    }
 
-#if defined(EEZ_PLATFORM_STM32)
-    void onSpiIrq() {
-        spiReady = true;
-    }
-#endif
+        if (currentCommand) {
+            if (!synchronized && currentCommand->command != COMMAND_GET_INFO) {
+                doCommandDone(false);
+            } else {
+                if (
+                    state == STATE_WAIT_SLAVE_READY_BEFORE_REQUEST ||
+                    state == STATE_WAIT_SLAVE_READY_BEFORE_RESPONSE
+                ) {
+                    #if defined(EEZ_PLATFORM_STM32)
+                	if (spiReady) {
+                		stateTransition(EVENT_SLAVE_READY);
+                	}
+                    #endif
 
-    bool transfer() {
-#if defined(EEZ_PLATFORM_STM32)
-        auto status = bp3c::comm::transfer(slotIndex, output, input, BUFFER_SIZE);
-        if (status == bp3c::comm::TRANSFER_STATUS_OK) {
-            numCrcErrors = 0;
-            lastTransferTickCount = millis();
-            return true;
+                    #if defined(EEZ_PLATFORM_SIMULATOR)
+                    stateTransition(EVENT_SLAVE_READY);
+                    #endif
+                } 
+                
+                if (
+                    state == STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_REQUEST ||
+                    state == STATE_WAIT_DMA_TRANSFER_COMPLETED_FOR_RESPONSE
+                ) {
+                
+                    #if defined(EEZ_PLATFORM_STM32)
+                    if (spiDmaTransferCompleted) {
+                        if (spiDmaTransferStatus == bp3c::comm::TRANSFER_STATUS_OK) {
+                            stateTransition(EVENT_DMA_TRANSFER_COMPLETED);
+                        } else {
+                            reportDmaTransferFailed(spiDmaTransferStatus);
+                            stateTransition(EVENT_DMA_TRANSFER_FAILED);
+                        }
+                    }
+                    #endif                
+
+                    #if defined(EEZ_PLATFORM_SIMULATOR)
+                    auto response = (Response *)input;
+
+                    response->command = 0x8000 | currentCommand->command;
+
+                    if (currentCommand->command == COMMAND_GET_INFO) {
+                        response->getInfo.firmwareMajorVersion = 1;
+                        response->getInfo.firmwareMinorVersion = 0;
+                        response->getInfo.idw0 = 0;
+                        response->getInfo.idw1 = 0;
+                        response->getInfo.idw2 = 0;
+                    }
+
+                    stateTransition(EVENT_DMA_TRANSFER_COMPLETED);
+                    #endif
+                } 
+                
+                uint32_t tickCountMs = millis();
+                if (tickCountMs - lastStateTransitionTime >= TIMEOUT_TIME_MS) {
+                    stateTransition(EVENT_TIMEOUT);
+                }
+            }
         } else {
-            if (status == bp3c::comm::TRANSFER_STATUS_CRC_ERROR) {
-                if (++numCrcErrors >= 10) {
-                    psu::event_queue::pushEvent(psu::event_queue::EVENT_ERROR_SLOT1_CRC_CHECK_ERROR + slotIndex);
+            if (synchronized) {
+            	uint32_t tickCountMs = millis();
+                if (tickCountMs - lastTransferTime >= TIMEOUT_UNTIL_OUT_OF_SYNC_MS) {
+                    event_queue::pushEvent(event_queue::EVENT_ERROR_SLOT1_SYNC_ERROR + slotIndex);
                     synchronized = false;
                     testResult = TEST_FAILED;
+                } else if (tickCountMs - lastRefreshTime >= getRefreshTimeMs()) {
+                    refreshStartTime = tickCountMs;
+                    executeCommand(&getState_command);
                 } else {
-                    DebugTrace("Slot %d CRC %d\n", slotIndex + 1, numCrcErrors);
+                    SetParams params;
+                    fillSetParams(params);
+                    if (memcmp(&params, &lastTransferredParams, sizeof(SetParams)) != 0) {
+                        executeCommand(&setParams_command);
+                    }
                 }
-            } else {
-                DebugTrace("Slot %d SPI transfer error %d\n", slotIndex + 1, status);
             }
-            return false;
         }
-#endif
-        return true;
     }
 
-    void writeUnsavedData() override {
-        saveRelayCycles();
+    void onSpiIrq() {
+        spiReady = true;
+		if (g_isBooted) {
+			stateTransition(EVENT_SLAVE_READY);
+		}
+    }
+
+    void onSpiDmaTransferCompleted(int status) override {
+         if (g_isBooted) {
+             if (status == bp3c::comm::TRANSFER_STATUS_OK) {
+                 stateTransition(EVENT_DMA_TRANSFER_COMPLETED);
+             } else {
+                 reportDmaTransferFailed(status);
+                 stateTransition(EVENT_DMA_TRANSFER_FAILED);
+             }
+         } else {
+             spiDmaTransferCompleted = true;
+             spiDmaTransferStatus = status;
+         }
     }
 
     void onPowerDown() override {
         synchronized = false;
+    }
+
+    void resync() override {
+        if (!synchronized) {
+            executeCommand(&getInfo_command);
+        }
+    }
+
+    void writeUnsavedData() override {
+        saveRelayCycles();
     }
 
     Page *getPageFromId(int pageId) override;
@@ -901,6 +1236,28 @@ public:
         }
     }
 };
+
+////////////////////////////////////////////////////////////////////////////////
+
+const Smx46Module::CommandDef Smx46Module::getInfo_command = {
+	COMMAND_GET_INFO,
+	nullptr,
+	&Smx46Module::Command_GetInfo_Done
+};
+
+const Smx46Module::CommandDef Smx46Module::getState_command = {
+	COMMAND_GET_STATE,
+	nullptr,
+	&Smx46Module::Command_GetState_Done
+};
+
+const Smx46Module::CommandDef Smx46Module::setParams_command = {
+	COMMAND_SET_PARAMS,
+	&Smx46Module::Command_SetParams_FillRequest,
+	&Smx46Module::Command_SetParams_Done
+};
+
+////////////////////////////////////////////////////////////////////////////////
 
 static Smx46Module g_smx46Module;
 Module *g_module = &g_smx46Module;
