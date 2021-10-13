9f2b0459fa8ffb5335d83a2bda11e7ed0fd0502d
Martin Vladic <martin.vladic@gmail.com>
2021-10-13 19:04:30 +0200

    #268

--- src/eez/modules/dib-mux14d/dib-mux14d.cpp
+++ src/eez/modules/dib-mux14d/dib-mux14d.cpp
@@ -326,7 +326,7 @@ public:
         adib2RelayState = params.adib2RelayState;
 		extRelayState = params.extRelayState;
 
-        return bp3c::comm::updateParamsFinish(&params, &lastTransferredParams, sizeof(SetParams), 200, err);
+        return bp3c::comm::updateParamsFinish(&params, &lastTransferredParams, sizeof(SetParams), TIMEOUT_UNTIL_OUT_OF_SYNC_MS, err);
     }
 
 	void fillSetParams(SetParams &params) {
--- src/eez/modules/dib-prel6/dib-prel6.cpp
+++ src/eez/modules/dib-prel6/dib-prel6.cpp
@@ -290,7 +290,7 @@ public:
 
         relayStates = params.relayStates;
 
-        return bp3c::comm::updateParamsFinish(&params, &lastTransferredParams, sizeof(SetParams), 200, err);
+        return bp3c::comm::updateParamsFinish(&params, &lastTransferredParams, sizeof(SetParams), TIMEOUT_UNTIL_OUT_OF_SYNC_MS, err);
     }
 
 	void fillSetParams(SetParams &params) {
